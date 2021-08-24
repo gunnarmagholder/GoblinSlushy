62f764982a995b011ddd83299a6e37392b59329c
Gunnar Magholder <gmagholder@gmail.com>
2021-08-24 17:49:01 +0200

    Try to get the rotation right

--- Entities/Arrow.gd
+++ Entities/Arrow.gd
@@ -8,7 +8,7 @@ func _ready():
 
 func _process(delta):
 	var velocity = get_linear_velocity()
-	rotation = atan2(velocity.x, velocity.y)
+	rotation = atan2(velocity.x, velocity.y) * -1
 	print(rotation)
 
 func SelfDestruct():
